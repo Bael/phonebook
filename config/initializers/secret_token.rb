c691eb2c67a16b0b4078dd409e46a8cbee5712e6
Dennis Kerner <denkern@gmail.com>
2013-09-01 17:25:02 +0800

    init empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebook::Application.config.secret_token = '14a560154f77f55b1259a4295bc1b13640d1fda8217cd97dfa25332140346445ff3f0759b6a99d5d324452b5cda28513b824184ee2a6f5ec6d10e34c387c3f5e'
