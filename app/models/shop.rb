63f18f923ccf888eb966de665961b224c7463bf5
Dennis Kerner <denkern@gmail.com>
2013-09-23 23:53:00 +0800

    try github app

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ActiveRecord::Base
   belongs_to :railway
-  attr_accessible :contract, :description, :email, :fax, :firstname, :icq, :lastname, :legaladdress, :name, :okpo, :owncellphone, :postaddress, :reception, :scancontract, :secondname, :station, :tgnl, :title, :workcellphone, :workphone
+  attr_accessible :contract, :description, :email, :fax, :firstname, :icq, 
+  :lastname, :legaladdress, :name, :okpo, :owncellphone, :postaddress, 
+  :reception, :scancontract, :secondname, :station, :tgnl, :title, :workcellphone, :workphone, :railway
 end
